23e076d1c3eec110d18af784ede80fc7560f0044
dennysbueno <dennys.bueno8@gmail.com>
2023-03-07 19:11:53 -0300

    :sparkles: feat: add migrations

---
+++ src/database/migrations/20230306223540_levels.ts
@@ -0,0 +1,15 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("levels", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable().unique();
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("levels");
+}
+
