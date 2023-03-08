cf726f7ec614eef457a5be760d5a379a70e834d7
dennysbueno <dennys.bueno8@gmail.com>
2023-03-08 11:08:57 -0300

    :lock: add .env

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const router = require("./routers");
 const cors = require("cors");
@@ -6,5 +7,5 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 app.use(router)
-app.listen(3333)
+app.listen(process.env.API_PORT || 3000)
 console.log("App em funcionamento!!")
