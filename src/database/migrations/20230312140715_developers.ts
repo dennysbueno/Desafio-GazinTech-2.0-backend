37e8a0fccc5e1d141f81b9dc1d5be53538956979
dennysbueno <dennys.bueno8@gmail.com>
2023-03-12 12:51:37 -0300

    :bricks: ci: developer table field swap

---
+++ src/database/migrations/20230312140715_developers.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("developers", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("birthdate").notNullable();
+    table.string("gender").notNullable();
+    table.string("team").notNullable();
+    table
+      .integer("levelId")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("levels")
+      .onDelete("RESTRICT");
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("developers");
+}
+
